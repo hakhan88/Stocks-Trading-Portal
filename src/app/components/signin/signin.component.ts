efe1e9bdc35bfbd3162edd0c989422bdf9bd4e4b
Haris Ali Khan <harisali.khan@thetasp.com>
2021-02-20 16:03:55 +0800

    fix filter open issue to show table for the data and login passwork issue

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+    selector: 'app-signin',
+    templateUrl: './signin.component.html',
+    styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+    hide = true;
+    hide2 = true;
+    constructor(private route: Router) { }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
+    signIn(): void {
+        this.route.navigate(['/stockDetails']);
+    }
 
 }
